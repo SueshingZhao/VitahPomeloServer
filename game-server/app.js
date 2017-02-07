f206f82cf4993ef373491fe556662dbfa3fb80b4
Vitah <linw1225@gmail.com>
2017-02-07 16:12:12 +0800

    增加与客户端通信配置

--- game-server/app.js
+++ game-server/app.js
@@ -7,22 +7,33 @@ var app = pomelo.createApp();
 app.set('name', 'VitahPomeloServer');
 
 // app configuration
-app.configure('production|development', 'connector', function(){
-  app.set('connectorConfig',
-    {
-      connector : pomelo.connectors.sioconnector,
-      //websocket, htmlfile, xhr-polling, jsonp-polling, flashsocket
-      transports : ['websocket'],
-      heartbeats : true,
-      closeTimeout : 60,
-      heartbeatTimeout : 60,
-      heartbeatInterval : 25
-    });
+
+// 原先配置
+
+app.configure('production|development', 'connector', function() {
+	app.set('connectorConfig', {
+		connector: pomelo.connectors.sioconnector,
+		//websocket, htmlfile, xhr-polling, jsonp-polling, flashsocket
+		transports: ['websocket'],
+		heartbeats: true,
+		closeTimeout: 60,
+		heartbeatTimeout: 60,
+		heartbeatInterval: 25
+	});
 });
+
+/*
+app.configure('production|development', 'connector', function() {
+	app.set('connectorConfig', {
+		connector: pomelo.connectors.hybridconnector,
+		heartbeat: 30
+	});
+});
+*/
 
 // start app
 app.start();
 
-process.on('uncaughtException', function (err) {
-  console.error(' Caught exception: ' + err.stack);
+process.on('uncaughtException', function(err) {
+	console.error(' Caught exception: ' + err.stack);
 });
