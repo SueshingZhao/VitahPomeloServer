ff7671990c36d5ee7cfac3c69b12de117031aa5d
Vitah <linw1225@gmail.com>
2017-02-14 15:36:27 +0800

    数据库配置和连接

--- game-server/app.js
+++ game-server/app.js
@@ -1,4 +1,6 @@
 var pomelo = require('pomelo');
+var mongoose = require('mongoose');
+var configUtil = require('./app/util/configUtil.js');
 
 /**
  * Init app for client.
@@ -7,6 +9,24 @@ var app = pomelo.createApp();
 app.set('name', 'VitahPomeloServer');
 
 // app configuration
+
+app.configure('all', function() {
+	// 载入mongodb数据库的配置
+	var mongodb_config = configUtil.load('mongodb');
+
+	// 使用mongoose连接mongodb
+	var db = mongoose.connect(mongodb_config.host, mongodb_config.database, mongodb_config.port, mongodb_config.options).connection;
+
+	// 当数据库连接失败和成功时的处理
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function callback() {
+		console.log('数据库连接成功');
+	});
+
+	// 监控请求响应时间，如果超时就给出警告
+	app.filter(pomelo.timeout());
+});
+
 
 // 原先配置
 /*
@@ -23,7 +43,9 @@ app.configure('production|development', 'connector', function() {
 });
 */
 
-app.configure('all', 'connector', function () {
+
+// 定义服务端和客户端的连接方式
+app.configure('all', 'connector', function() {
 	app.set('connectorConfig', {
 		connector: pomelo.connectors.hybridconnector,
 		heartbeat: 30
@@ -33,6 +55,6 @@ app.configure('all', 'connector', function () {
 // start app
 app.start();
 
-process.on('uncaughtException', function (err) {
+process.on('uncaughtException', function(err) {
 	console.error(' Caught exception: ' + err.stack);
 });
