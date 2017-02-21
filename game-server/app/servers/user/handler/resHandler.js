bbc0a65ec5b943cddff2830fe2345064fe9e3e91
Vitah <linw1225@gmail.com>
2017-02-21 11:27:02 +0800

    增加资源增加协议

---
+++ game-server/app/servers/user/handler/resHandler.js
@@ -0,0 +1,55 @@
+var co = require('co');
+var thunkify = require('thunkify');
+var roleModel = require('../../../models/roleModel.js');
+var code = require('../../../consts/code.js');
+var pushService = require('../../../services/pushService.js');
+
+/////////////////////////////////////////////////////////////////
+
+module.exports = function(app) {
+	return new Handler(app);
+};
+
+var Handler = function(app) {
+	this.app = app;
+};
+
+/**
+ * 获取玩家信息协议
+ * @return {[type]} [description]
+ */
+Handler.prototype.add = function(msg, session, next) {
+	var gold = msg.gold || 0;
+	var diamond = msg.diamond || 0;
+	var uid = session.uid;
+	if (!uid) {
+		return next(null, {
+			code: code.PARAM_ERROR
+		});
+	}
+
+	var onDo = function*() {
+		// 获取对应Uid的model
+		var role_model = yield thunkify(roleModel.getByUid)(uid);
+		var role_old_json = role_model.toJSON();
+		role_model.addGold(gold);
+		role_model.addDiamond(diamond);
+		yield thunkify(role_model.save)();
+
+		// 推送
+		pushService.pushRoleModify(uid, role_old_json, role_model.toJSON());
+
+		return next(null, {
+			code: code.OK
+		});
+	};
+
+	var onError = function(err) {
+		console.error(err);
+		return next(null, {
+			code: code.FAIL
+		});
+	};
+
+	co(onDo).catch(onError);
+};
